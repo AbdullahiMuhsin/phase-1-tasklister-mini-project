f78852b9fc95a76c631747eb1b1f00818f73b98d
abdullahi abdi <abdullahimuxsin398@gmail.com>
2024-07-13 14:04:14 +0300

    Tried

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded',() => {
+const form = document.getElementById('create-task-form');
+const tasksList = document.getElementById('tasks');
+
+// Event listener for form submission
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+ 
+  const taskDescription = document.getElementById('new-task-description').value;
+
+  
+  const newTaskItem = document.createElement('li');
+  newTaskItem.textContent = taskDescription; 
+
+  
+  
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('delete-button'); 
+
+  
+  deleteButton.addEventListener('click', function () {
+      tasksList.removeChild(newTaskItem); 
+  });
+  newTaskItem.appendChild(deleteButton); 
+
+  tasksList.appendChild(newTaskItem);
+document.getElementById('new-task-description').value = '';
+  
+});
 });
